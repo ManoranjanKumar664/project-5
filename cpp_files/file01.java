43e39b2da7f43b1d1a1f2a71c7b478acedb13295
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2025-10-28 11:02:37 +0530

    Add files via upload

---
+++ cpp_files/file01.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class file01
+{
+    public static void main(String []args)
+    {
+        int a[]={49, 3, 9};
+        /*System.out.println("enter any five element=");
+        Scanner s=new Scanner(System.in);
+        for(int i=0;i<5;i++) 
+        {
+            a[i]=s.nextInt();
+        }
+        */
+        System.out.println("element are:");
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+}
